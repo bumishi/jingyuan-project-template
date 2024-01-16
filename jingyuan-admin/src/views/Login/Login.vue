104080bdc3d9f66b2f2b89ca90c300575a887b4b
xieqiang <1060420897@qq.com>
2024-01-16 23:37:28 +0800

    logo src add base_url

--- jingyuan-admin/src/views/Login/Login.vue
+++ jingyuan-admin/src/views/Login/Login.vue
@@ -17,6 +17,7 @@ const prefixCls = getPrefixCls('login')
 const appStore = useAppStore()
 
 const { t } = useI18n()
+const BASE = import.meta.env.VITE_API_BASE_PATH;
 
 const isPasswordLogin = ref(true)
 const logo = computed(() => appStore.getLogoImage)
@@ -32,13 +33,13 @@ const toPasswordLogin = () => {
 
 <template>
   <div
-    :class="prefixCls"
-    class="h-[100%] relative lt-xl:bg-[var(--login-bg-color)] lt-sm:px-10px lt-xl:px-10px lt-md:px-10px"
+      :class="prefixCls"
+      class="h-[100%] relative lt-xl:bg-[var(--login-bg-color)] lt-sm:px-10px lt-xl:px-10px lt-md:px-10px"
   >
     <ElScrollbar class="h-full">
       <div class="relative flex mx-auto min-h-100vh">
         <div
-          :class="`${prefixCls}__left flex-1 bg-gray-500 bg-opacity-20 relative p-30px lt-xl:hidden`"
+            :class="`${prefixCls}__left flex-1 bg-gray-500 bg-opacity-20 relative p-30px lt-xl:hidden`"
         >
           <div class="flex items-center relative text-white">
             <img :src="logo" alt="" class="w-48px h-48px mr-10px" />
@@ -46,9 +47,9 @@ const toPasswordLogin = () => {
           </div>
           <div class="flex justify-center items-center h-[calc(100%-60px)]">
             <TransitionGroup
-              appear
-              tag="div"
-              enter-active-class="animate__animated animate__bounceInLeft"
+                appear
+                tag="div"
+                enter-active-class="animate__animated animate__bounceInLeft"
             >
               <img src="@/assets/svgs/login-box-bg.svg" key="1" alt="" class="w-350px" />
               <div class="text-3xl text-white" key="2">{{ t('login.welcome') }}</div>
@@ -60,10 +61,10 @@ const toPasswordLogin = () => {
         </div>
         <div class="flex-1 p-30px lt-sm:p-10px dark:bg-[var(--login-bg-color)] relative">
           <div
-            class="flex justify-between items-center text-white at-2xl:justify-end at-xl:justify-end"
+              class="flex justify-between items-center text-white at-2xl:justify-end at-xl:justify-end"
           >
             <div class="flex items-center at-2xl:hidden at-xl:hidden">
-              <img :src="logo" alt="" class="w-48px h-48px mr-10px" />
+              <img :src="BASE+logo" alt="" class="w-48px h-48px mr-10px" />
               <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
             </div>
 
@@ -74,17 +75,17 @@ const toPasswordLogin = () => {
           </div>
           <Transition appear enter-active-class="animate__animated animate__bounceInRight">
             <div
-              class="h-full flex items-center m-auto w-[100%] at-2xl:max-w-500px at-xl:max-w-500px at-md:max-w-500px at-lg:max-w-500px"
+                class="h-full flex items-center m-auto w-[100%] at-2xl:max-w-500px at-xl:max-w-500px at-md:max-w-500px at-lg:max-w-500px"
             >
               <LoginForm
-                v-if="isPasswordLogin"
-                class="p-20px h-auto m-auto lt-xl:rounded-3xl lt-xl:light:bg-white"
-                @to-telephone="toTelephoneLogin"
+                  v-if="isPasswordLogin"
+                  class="p-20px h-auto m-auto lt-xl:rounded-3xl lt-xl:light:bg-white"
+                  @to-telephone="toTelephoneLogin"
               />
               <TelephoneCodeForm
-                v-else
-                class="p-20px h-auto m-auto lt-xl:rounded-3xl lt-xl:light:bg-white"
-                @to-password="toPasswordLogin"
+                  v-else
+                  class="p-20px h-auto m-auto lt-xl:rounded-3xl lt-xl:light:bg-white"
+                  @to-password="toPasswordLogin"
               />
             </div>
           </Transition>
